de388aaa7235ac2e681381d12765e7130e8026bd
Michael Wilson <mawilson0410@gmail.com>
2023-10-31 18:57:52 -0400

    modified:   ModernCrown.Api/Controllers/CatalogController.cs added http put and delete

--- ModernCrown.Api/Controllers/CatalogController.cs
+++ ModernCrown.Api/Controllers/CatalogController.cs
@@ -9,41 +9,53 @@ namespace Modern.Crown.Api.Controllers
     public class CatalogController : ControllerBase
     {
 
-    [HttpGet]
-    public IActionResult GetItems()
-    {
-        var items = new List <Item>()
+        [HttpGet]
+        public IActionResult GetItems()
         {
-            new Item ("Shirt", "Ohio State shirt.", "Nike", 29.99m),
-            new Item ("Shorts", "Ohio State shorts.", "Nike", 44.99m)
-        };
-        return Ok(items);
-    }
+            var items = new List <Item>()
+            {
+                new Item ("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+                new Item ("Shorts", "Ohio State shorts.", "Nike", 44.99m)
+            };
+            return Ok(items);
+        }
 
-    [HttpGet("{id:int}")]
-    public IActionResult GetItem (int id)
-    {
-        var item = new Item ("Shirt", "Ohio State shirt.", "Nike", 29.99m);
-        item.Id = id;
+        [HttpGet("{id:int}")]
+        public IActionResult GetItem (int id)
+        {
+            var item = new Item ("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+            item.Id = id;
 
-        return Ok (item);
-    }
+            return Ok (item);
+        }
 
-    [HttpPost]
-    public IActionResult Post(Item item)
-    {
-        return Created("/catalog/42", item);
-    }
+        [HttpPost]
+        public IActionResult Post(Item item)
+        {
+            return Created("/catalog/42", item);
+        }
 
-    [HttpPost("{id:int}/ratings")]
-    public IActionResult PostRating(int id, [FromBody] Rating rating)
-    {
-        var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
-        item.Id = id;
-        item.AddRating(rating);
+        [HttpPost("{id:int}/ratings")]
+        public IActionResult PostRating(int id, [FromBody] Rating rating)
+        {
+            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+            item.Id = id;
+            item.AddRating(rating);
 
-        return Ok(item);
-    }
+            return Ok(item);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id, Item item)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public  IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
 
     }
     
